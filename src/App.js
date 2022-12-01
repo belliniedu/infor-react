742d01d49be90b33ea76f20785fb17a5cc095b7a
belliniedu <belliniedu@gmail.com>
2022-12-01 20:46:07 -0300

    seguimos

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Hola Mundi</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
