92a502aff10d1b287984563a31328551fce99211
belliniedu <belliniedu@gmail.com>
2022-12-01 22:29:57 -0300

    prueba1

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return (
+        <h2>LOGO</h2>
+    )
+
+
+};
+
+export default Logo;
---
+++ src/paginas/PaginaBuscador.jsx
@@ -0,0 +1,14 @@
+
+import Buscador from '../components/Buscador';
+
+const PaginaBuscador = () => {
+    return ( 
+
+            <div><Buscador /></div>
+
+    )
+
+
+};
+
+export default PaginaBuscador;
